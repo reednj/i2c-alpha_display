e0fbcc775834709fe70b79b5e2ba6813aa1d4a9e
Nathan Reed <reednj@gmail.com>
2017-01-05 19:58:59 +0800

    initial commit

---
+++ lib/i2c/alpha_display.rb
@@ -0,0 +1,7 @@
+require "i2c/alpha_display/version"
+
+module I2c
+  module AlphaDisplay
+    # Your code goes here...
+  end
+end
---
+++ lib/i2c/alpha_display/version.rb
@@ -0,0 +1,5 @@
+module I2c
+  module AlphaDisplay
+    VERSION = "0.1.0"
+  end
+end
